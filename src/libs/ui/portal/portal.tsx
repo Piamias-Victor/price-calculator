ee028d1f7fe18781e52420b651715dbdd8b0406b
ChocoDev23 <chocowalletdev@proton.me>
2024-03-13 11:44:07 +0100

    Push

--- src/libs/ui/portal/portal.tsx
+++ src/libs/ui/portal/portal.tsx
@@ -1,30 +1,19 @@
 import { useLazyMemo } from "@/libs/react/memo"
 import { ChildrenProps } from "@/libs/react/props/children"
-import { NullableElementProps } from "@/libs/react/props/element"
 import { useEffect } from "react"
 import { createPortal } from "react-dom"
 import { TypeProps } from "../../react/props/type"
 
-export function Portal(props: TypeProps & NullableElementProps<"container"> & ChildrenProps) {
+export function Portal(props: TypeProps  & ChildrenProps) {
   const {
     children,
     type,
-    container = document.getElementById("__next")
   } = props
 
   const element = useLazyMemo(() => {
     return document.createElement(type)
   }, [type])
 
-  useEffect(() => {
-    if (element == null)
-      return
-    if (container == null)
-      return
-
-    container.appendChild(element)
-    return () => void container.removeChild(element)
-  }, [element, container])
 
   if (element == null)
     return null
@@ -36,7 +25,7 @@ export namespace Portal {
 
   export function Test() {
     return <div className="p-1">
-      <Portal type="div" container={document.getElementById("__next")}>
+      <Portal type="div">
         Hello world
       </Portal>
       <div className="">
